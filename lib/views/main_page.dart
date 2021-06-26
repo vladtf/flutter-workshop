109b9ce0789474d451f494ad314f5c1ff32d6e11
Vladislav Tiftilov <vladislav.tiftilov@eu.rsystems.com>
2021-06-26 23:30:59 +0300

    Add pie_chard

--- lib/views/main_page.dart
+++ lib/views/main_page.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:pie_chart/pie_chart.dart';
 
 class MainPage extends StatelessWidget {
+  Map<String, double> dataMap = {
+    "Flutter": 5,
+    "React": 3,
+    "Xamarin": 2,
+    "Ionic": 2,
+  };
+
   @override
   Widget build(BuildContext context) {
     var appBar = AppBar(
@@ -9,11 +17,25 @@ class MainPage extends StatelessWidget {
     );
     return Scaffold(
       appBar: appBar,
-      body: Card(
-        child: Container(
-          color: Colors.redAccent,
-          height: (MediaQuery.of(context).size.height -
-                  appBar.preferredSize.height) / 3,
+      body: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Card(
+                child: PieChart(dataMap: dataMap),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Card(
+                child: Container(
+                  color: Colors.blueAccent,
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
