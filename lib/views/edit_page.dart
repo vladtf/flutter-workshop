6cc3b081f0b441cb20fc40a088a018f864053007
vladtf <vendeta0263@gmail.com>
2021-06-27 20:58:42 +0300

    Add total to editing page

--- lib/views/edit_page.dart
+++ lib/views/edit_page.dart
@@ -42,39 +42,38 @@ class _EditPageState extends State<EditPage> {
       appBar: appBar,
       body: Padding(
         padding: EdgeInsets.all(8.0),
-        child: ListView(
-          children: [
-                Card(
-                  child: PieChart(dataMap: dataMap),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          Expanded(
-                            flex: 3,
-                            child: Text(
-                              "Name",
-                              style: Theme.of(context).textTheme.headline6,
-                            ),
-                          ),
-                          Expanded(
-                            flex: 1,
-                            child: Text(
-                              "Value",
-                              style: Theme.of(context).textTheme.headline6,
-                            ),
-                          )
-                        ],
+        child: ListView(children: [
+          Card(
+            child: PieChart(dataMap: dataMap),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      flex: 3,
+                      child: Text(
+                        "Name",
+                        style: Theme.of(context).textTheme.headline6,
                       ),
-                    ],
-                  ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Text(
+                        "Value",
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                    )
+                  ],
                 ),
-              ] +
-              buildTextFields(dataMap, context),
-        ),
+              ],
+            ),
+          ),
+          Divider(),
+          Column(children: buildTextFields(dataMap, context))
+        ]),
       ),
     );
   }
@@ -123,6 +122,35 @@ class _EditPageState extends State<EditPage> {
             ))
         .toList();
 
+    rows.add(Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 3,
+            child: Focus(
+              child: Text(
+                "Total: ",
+                style: Theme.of(context).textTheme.headline6,
+                textAlign: TextAlign.right,
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 16.0,
+          ),
+          Expanded(
+            flex: 1,
+            child: TextFormField(
+              initialValue: dataMap.values
+                  .fold(0, (previousValue, element) => previousValue + element)
+                  .toString(),
+              readOnly: true,
+            ),
+          )
+        ],
+      ),
+    ));
     return rows;
   }
 
